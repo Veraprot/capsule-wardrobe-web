84322ed209ca57762b5b1d13774e587a99ed6992
minheekangg <minheekangg@gmail.com>
2018-12-06 21:39:01 -0500

    some styling

--- src/mh/index.js
+++ src/mh/index.js
@@ -15,23 +15,25 @@ document.addEventListener('DOMContentLoaded', () => {
   let flkty = new Flickity( '.carousel', {
     initialIndex: 1
   });
-  
+
   let appendButton = document.querySelector('.button--append');
   appendButton.addEventListener( 'click', function() {
     var cellElems = [ makeCell()];
     flkty.append( cellElems );
   });
-  
+
   let cellCount = flkty.cells.length;
-  
+
   function makeCell() {
     if(outfit.innerHTML.trim() !== "") {
       cellCount++;
       let cell = document.createElement('div');
       cell.className = 'outfit-cell';
-      cell.innerHTML = outfit.innerHTML;
+      console.log(outfit);
+      cell.innerHTML =
+        Array.from(outfit.getElementsByTagName('img')).map(e => `<img src="${e.src}">`).join('')
       return cell;
-    } 
+    }
     else {
       outfit.innerHTML = "please add some items";
     }
